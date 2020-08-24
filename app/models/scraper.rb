df783d3d546bbf2d2738cb94464de7a67a0d9f96
wickbushong <wickbushong@gmail.com>
2020-08-24 09:20:19 -0400

    Add empty models

---
+++ app/models/scraper.rb
@@ -0,0 +1,15 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+class Scraper
+    def initialize
+        @doc = Nokogiri::HTML(open("https://www.daftlogic.com/information-appliance-power-consumption.htm"))
+    end
+
+    def inspect
+        binding.pry
+    end
+end
+
+Scraper.new.inspect
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 ENV['SINATRA_ENV'] ||= "development"
 
+require 'open-uri'
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
