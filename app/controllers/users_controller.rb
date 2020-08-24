cd093e177ae4bb70d25c79bee2c49213199cc376
wickbushong <wickbushong@gmail.com>
2020-08-24 13:24:18 -0400

    Authenticates login inputs.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,15 @@ class UsersController < ApplicationController
     end
 
     post '/login' do
-        binding.pry
+        @user = User.find_by(:username => params[:username])
+ 
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect "/users/#{@user.id}"
+        else
+            # message: invalid username or pass
+            redirect '/login'
+        end
     end
 
     get '/users/:id' do
