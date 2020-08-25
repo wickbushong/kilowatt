d0b6657fba8d83905e9cdf1222fdefc3c2534ebb
wickbushong <wickbushong@gmail.com>
2020-08-25 16:04:23 -0400

    Seed group options.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,10 @@
+Option.destroy_all
+Group.destroy_all
+
 Scraper.call
 
+groups = ["Lighting", "Kitchen", "Living Room", "Bedroom", "Garage", "Backyard", "Laundry", "Bathroom", "Climate Control", ]
+
+groups.each do |g|
+    Group.create(name: g)
+end
