212ea6c324ce9e380cbf1259e549bc897678b3d5
wickbushong <wickbushong@gmail.com>
2020-08-26 16:03:57 -0400

    Establish edit group route.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -28,6 +28,18 @@ class GroupsController < ApplicationController
         redirect '/devices/new'
     end
     
+    get '/groups/:id/edit' do
+        @group = Group.find(params[:id])
+        if !logged_in?
+            flash[:error] = "Must be logged in to view groups"
+            redirect '/login'
+        elsif !current_user.groups.include?(@group)
+            flash[:error] = "That group belongs to another user"
+            redirect '/groups'
+        end
+        erb :'groups/edit'
+    end
+    
     get '/groups/:id' do
         @group = Group.find(params[:id])
         if !logged_in?
