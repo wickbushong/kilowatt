e76add26ea57a41214a107bfc78dd1551f4ecade
wickbushong <wickbushong@gmail.com>
2020-08-26 14:21:30 -0400

    Adjust seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,13 @@
+User.destroy_all
+Device.destroy_all
+DeviceGroup.destroy_all
 Group.destroy_all
 Option.destroy_all
 
 Scraper.call
 
-groups = ["Lighting", "Kitchen", "Living Room", "Bedroom", "Garage", "Backyard", "Laundry", "Bathroom", "Climate Control", ]
+# groups = ["Lighting", "Kitchen", "Living Room", "Bedroom", "Garage", "Backyard", "Laundry", "Bathroom", "Climate Control", ]
 
-groups.each do |g|
-    Group.create(name: g)
-end
+# groups.each do |g|
+#     Group.create(name: g)
+# end
