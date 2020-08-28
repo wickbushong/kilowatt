af87a994025a9b9f80fe17e79280d5724345c4eb
wickbushong <wickbushong@gmail.com>
2020-08-28 10:32:10 -0400

    Round helper consumption methods. Add consumtion to home page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,7 +31,7 @@ class ApplicationController < Sinatra::Base
     end
 
     def group_consumption(group)
-      group.devices.collect{|device| daily_consumption(device)}.inject(0, :+)
+      group.devices.collect{|device| daily_consumption(device)}.inject(0, :+).round(5)
     end
 
   end
