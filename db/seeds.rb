da11afc3f910132a107c43ddaa1f01b8314c53d5
wickbushong <wickbushong@gmail.com>
2020-08-30 10:51:11 -0400

    Adjust seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,3 @@
-# User.destroy_all
-# Device.destroy_all
-# DeviceGroup.destroy_all
-# Group.destroy_all
-# Option.destroy_all
-
 Scraper.call
 
-User.create(
-    username: "wick",
-    email: "blah@fake.com",
-    password: "1234"
-)
 
-User.create(
-    username: "test2",
-    email: "test@two.com",
-    password: "pass2"
-)
-# groups = ["Lighting", "Kitchen", "Living Room", "Bedroom", "Garage", "Backyard", "Laundry", "Bathroom", "Climate Control", ]
-
-# groups.each do |g|
-#     Group.create(name: g)
-# end
